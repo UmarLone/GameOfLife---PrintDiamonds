278770335ed0075a577f83962298c840f88292ec
Umar Lone <07umarjan@gmail.com>
2015-12-26 17:49:55 -0500

    Game-of-Life--&--Print Diamonds
    Game-of-Life & Print Diamonds using C# console application

---
+++ GameofLife/ConsoleApplication10/Program.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameofLife
+{
+    
+        
+        
+public class Game
+
+{
+
+    static void Main(string[] args)
+    {
+
+
+        Console.WriteLine("|Game of Life|");
+        string result = new Game().NextGeneration("........\n....*...\n...**...\n........");
+
+        Console.WriteLine(result);
+
+        
+      
+
+
+
+    
+    }
+
+
+    // <summary>
+    /// Represents a game of life board
+    /// </summary>
+
+    private bool[,] originalArray;
+    private bool[,] resultArray;
+    private int width;
+    private int height;
+
+
+    public string NextGeneration(string input)
+    {
+        this.GenerateArrays(input);
+        for(var x = 0; x < width; x++)
+        {
+            for(var y = 0; y < height; y++)
+            {
+                this.resultArray[x, y] = TestCell(x, y);
+            }
+        }
+        
+
+        return GenerateResult();
+    }
+
+    private bool TestCell(int x, int y)
+    {
+        var neighbours = CountNeighbours(x, y);
+        if (originalArray[x, y])
+        {
+           if (neighbours == 2)
+                return true;
+            if (neighbours == 3)
+                return true;
+        }
+        else if(neighbours == 3)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    private int CountNeighbours(int x, int y)
+    {
+        int result = 0;
+        if (SafeIsNeighbour(x - 1, y)) result += 1;
+        if (SafeIsNeighbour(x + 1, y)) result += 1;
+        if (SafeIsNeighbour(x, y - 1)) result += 1;
+        if (SafeIsNeighbour(x, y + 1)) result += 1;
+        if (SafeIsNeighbour(x - 1, y - 1)) result += 1;
+        if (SafeIsNeighbour(x + 1, y + 1)) result += 1;
+        if (SafeIsNeighbour(x + 1, y - 1)) result += 1;
+        if (SafeIsNeighbour(x - 1, y + 1)) result += 1;
+        return result;
+    }
+
+    private bool SafeIsNeighbour(int x, int y)
+    {
+        if (x < 0) return false;
+        if (x >= width) return false;
+        if (y < 0) return false;
+        if (y >= height) return false;
+        return originalArray[x, y];
+    }
+
+    private string GenerateResult()
+    {
+        var lines = new List<string>();
+        for(var y = 0; y < height; y++)
+        {
+            string line = "";
+            for(var x = 0; x < width; x++)
+            {
+                var field = resultArray[x,y] ? "*" : ".";
+                line += field;
+
+            }
+            lines.Add(line);
+        }
+        return string.Join("\n", lines.ToArray());
+    }
+
+    private void GenerateArrays(string input)
+    {
+        string[] lines = input.Split('\n');
+        this.width = lines[0].Length;
+        this.height = lines.Length;
+        
+        originalArray = new bool[this.width, this.height];        
+        resultArray = new bool[this.width, this.height];        
+        for(int x = 0; x < this.width; x++)
+        {
+            for(int y = 0; y < this.height; y++)
+            {
+                string line = lines[y];
+                originalArray[x,y] = line[x] == '*';
+            }
+
+            
+        }
+    }
+}
+
+
+
+
+
+
+    }
+
---
+++ GameofLife/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GameofLife;
+using NUnit.Framework;
+
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string input = "*.\n..";
+            string result = new Game().NextGeneration(input);
+            string expected = "..\n..";
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(expected, result);
+
+        }
+
+        [TestMethod]
+    public void CellWithTwoLivingNeighboursLives()
+    {
+        string input = "***";
+        string result = new Game().NextGeneration(input);
+        string expected = ".*.";
+        Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(expected, result);
+    }
+
+
+        [TestMethod]
+        public void CellWithTwoLivingInOneColumnNeighboursLives()
+        {
+            string input = "*\n*\n*";
+            string result = new Game().NextGeneration(input);
+            string expected = ".\n*\n.";
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(expected, result);
+        }
+
+
+        [TestMethod]
+        public void CellWithTwoLivingDiagonallyArrangedNeighboursLives()
+        {
+            string input = "*..\n.*.\n..*";
+            string result = new Game().NextGeneration(input);
+            string expected = "...\n.*.\n...";
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(expected, result);
+        }
+
+
+        [TestMethod]
+        public void CellWithTwoLivingDiagonallyArranged2NeighboursLives()
+        {
+            string input = "..*\n.*.\n*..";
+            string result = new Game().NextGeneration(input);
+            string expected = "...\n.*.\n...";
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(expected, result);
+        }
+
+
+        [TestMethod]
+        public void CellWithThreeLivingNeighboursLives()
+        {
+            string input = "**\n**";
+            string result = new Game().NextGeneration(input);
+            string expected = "**\n**";
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(expected, result);
+        }
+
+
+        [TestMethod]
+        public void CellWithMoreThanThreeIn3x3GridLivingNeighboursDies()
+        {
+            string input = "***\n.*.\n***";
+            string result = new Game().NextGeneration(input);
+            string expected = "***\n...\n***";
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(expected, result);
+        }
+
+
+        [TestMethod]
+      
+        public void DeadCellWithThreeNeighboursIn4x8GridLivingNeighboursLives()
+        {
+            string input = "........\n....*...\n...**...\n........";
+            string result = new Game().NextGeneration(input);
+            string expected = "........\n...**...\n...**...\n........";
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(expected, result);
+        }
+
+}
+
+
+
+
+
+    }
+
